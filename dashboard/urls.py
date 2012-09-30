cf22bfa47c08d2266c2d0510eeebc62107742a24
Luke O'Malley <omalley1@mit.edu>
2012-09-30 15:12:33 -0400

    dashboard: added sevenk link

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^$', 'dashboard.views.home'),
+    url(r'^sevenk$', 'dashboard.views.sevenk'),
     url(r'^events/', include('events.urls')),
     url(r'^nextbus/', include('nextbus.urls')),
     url(r'^news/', include('news.urls')),
--- dashboard/views.py
+++ dashboard/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render_to_response
 
 def home(request):
     return render_to_response('index.html', context_instance = RequestContext(request))
+
+def sevenk(request):
+    return render_to_response('sevenk.html', context_instance = RequestContext(request))
