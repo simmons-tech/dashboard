e9ace220d90d573bd651f3012013382570e98e62
Will Oursler <woursler@mit.edu>
2013-09-15 23:07:11 -0400

    Actually makes lightweight version work.

--- dashboard/views.py
+++ dashboard/views.py
@@ -47,4 +47,20 @@ def sevenk(request):
     return render_to_response('sevenk.html', context_instance = RequestContext(request))
 
 def lightweight(request):
-    return render_to_response('lightweight.html', context_instance = RequestContext(request))
+    	class Slide():
+		def __init__(self, slide_type, path="NA"):
+			# slide_type is of WIDGET or POSTER
+			self.stype = slide_type
+			self.path = path
+
+	slides = []
+	img_urls = requests.get("https://adat.scripts.mit.edu:444/www/dev/simmons/display/dashboard.php").json()
+
+	# naively insert a widget slide after every 2 images
+	for i in range(len(img_urls)):
+		if i % 2:
+			slides.append(Slide("WIDGET"))
+		slides.append(Slide("POSTER", img_urls[i]))
+	slides.append(Slide("PACKAGE-LIST"))
+
+	return render(request, "lightweight.html", {"slides": slides})
